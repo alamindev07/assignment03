d8869088b22565b8a0fec7bfb4cf7e441c599a76
alamindev07 <alamin.dev07@gmail.com>
2025-03-07 20:43:13 +0600

    service and popular section done with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+
 module.exports = {
-    theme: {
-      extend: {
-        backgroundImage: {
-          'custom-gradient': 'linear-gradient(to right, #CCFFFF, #FFFFFF)',
-        },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #CCFFFF, #FFFFFF)',
+      },
+      colors: {
+        'custom-beige': '#EFEBE3', // custom color
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
